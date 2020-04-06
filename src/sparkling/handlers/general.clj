50d82dc3bf02d461350511375c95c738e0110da9
Daniel Leong <me@dhleong.net>
2020-04-05 20:42:16 -0400

    Add very basic :initialize response + simplify notification sending

--- src/sparkling/handlers/general.clj
+++ src/sparkling/handlers/general.clj
@@ -1,5 +1,7 @@
 (ns sparkling.handlers.general
   (:require [sparkling.handlers.core :refer [defhandler]]))
 
-(defhandler :initialize []
-  (println "INITIALIZED"))
+(defhandler :initialize [{:keys [clientInfo capabilities rootPath]}]
+  {:capabilities {}
+   :serverInfo {:name "sparkling"
+                :version "SNAPSHOT"}})
--- src/sparkling/lsp.clj
+++ src/sparkling/lsp.clj
@@ -12,15 +12,34 @@
       lsp
       (:value lsp))))
 
+(defn- clean-message [message]
+  (update message :method (fn [m]
+                            (if (string? m)
+                              m
+                              (str (namespace m)
+                                   "/"
+                                   (name m))))))
 
 ; ======= public api ======================================
 
 (defn cancel-request! [request-id]
   ((:cancel! (instance)) request-id))
 
-(defn notify! [message]
-  (if-let [send! (:notify (instance))]
-    (send! message)
-    (println "WARN: *lsp* not started yet")))
+(defn notify!
+  ([method params] (notify! {:method method
+                             :params params}))
+  ([message]
+   (if-let [send! (:notify (instance))]
+     (send! (clean-message message))
+     (println "WARN: *lsp* not started yet"))))
 
-
+(defn log!
+  ([message] (log! :log message))
+  ([msg-type message]
+   (notify! :window/showMessage
+            {:type (case msg-type
+                     :error 1
+                     :warn 2
+                     :info 3
+                     4)
+             :message message})))
