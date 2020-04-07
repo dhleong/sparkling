fa2008c600fc5577830c99246e9e93f4d63088b8
Daniel Leong <me@dhleong.net>
2020-04-07 12:06:41 -0400

    Attempt to extract syntax error location

--- src/sparkling/nrepl/lsp.clj
+++ src/sparkling/nrepl/lsp.clj
@@ -3,14 +3,30 @@
 
 (def diagnostic-severity-error 1)
 
+(defn- clean-split-error [[line col msg]]
+  [(Integer/parseInt line)
+   (Integer/parseInt col)
+   msg])
+
+(defn split-error-message [message]
+  (some->>
+    (or (when-let [[_ & match] (re-find #"at \(.*?:(\d+):(\d+)\).\s*(.*)$"
+                                          message)]
+          match))
+    clean-split-error))
+
 (defn parse-diagnostic [^Throwable e]
-  ; TODO
-  {:range {:start {:line 0
-                   :character 0}
-           :end {:line 0
-                 :character 0}}
-   :source "nrepl"
+  (let [message (ex-message e)
+        [l c m] (or (split-error-message message)
+                    [0 0 message])]
+    {:source "nrepl:sparkling"
+     :severity diagnostic-severity-error
 
-   :message (ex-message e)
+     :message m
 
-   :severity diagnostic-severity-error})
+     :range {:start {:line l
+                     :character c}
+
+             ; TODO: better positioning?
+             :end {:line l
+                   :character c}}}))
